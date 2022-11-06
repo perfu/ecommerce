47c08b782d6b59376a56dfa5bda545f355de2292
perfu <renzvanni626@gmail.com>
2022-11-07 00:35:38 +0800

    drop changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 const cartSvgEl = document.querySelector(".header__div2-svg") 
 const cartDropEl = document.querySelector(".main__empty-cart")
 
-cartSvgEl.addEventListener("click", () => {
+// cartSvgEl.addEventListener("click", () => {
   
-  if(cartDropEl.style.display !== "none") {
-    cartDropEl.style.display = "none"
-  }
-  else {
-    cartDropEl.style.display = "inline"
-  }
+//   if(cartDropEl.style.display !== "none") {
+//     cartDropEl.style.display = "none"
+//   }
+//   else {
+//     cartDropEl.style.display = "inline"
+//   }
+  
+// })
+
+cartSvgEl.addEventListener("click", () => {
+  cartDropEl.classList.toggle("toggle")
   
 })
 
